cbf54593173c1b6f08d99ac4fe64b24db37459fb
Aleksandar Minchev <aminchev@protonmail.com>
2024-10-17 15:27:46 +0000

    firstTestCommit

--- student_info.sh
+++ student_info.sh
@@ -7,3 +7,5 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "First name, last name, and GPA of students with a 4.0 GPA:"
+
+#proba
