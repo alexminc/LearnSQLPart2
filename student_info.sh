af3ff1435bff7333d897c76f84377fce18c80814
Aleksandar Minchev <aminchev@protonmail.com>
2024-10-17 15:33:25 +0000

    Save progress: 1360.1

--- student_info.sh
+++ student_info.sh
@@ -7,3 +7,5 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
+
+echo "$($PSQL "select first_name, last_name, gpa from students where gpa = 4.0;")"
