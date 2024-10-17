9648c63eb9c758ad39c8be5410712ac6105face9
Aleksandar Minchev <aminchev@protonmail.com>
2024-10-17 15:31:05 +0000

    Save progress: 1320.1

--- student_info.sh
+++ student_info.sh
@@ -6,6 +6,4 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
-echo -e "First name, last name, and GPA of students with a 4.0 GPA:"
-
-#proba
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
