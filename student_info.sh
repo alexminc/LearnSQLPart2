9cc697b99a83929d8653264c95a90fae46fb89d6
Aleksandar Minchev <aminchev@protonmail.com>
2024-10-17 15:21:23 +0000

    Save progress: 1280.1

---
+++ student_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Info about my computer science students from students database
+
+echo -e "\n~~ My Computer Science Students ~~\n"
+
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+echo -e "First name, last name, and GPA of students with a 4.0 GPA:"
